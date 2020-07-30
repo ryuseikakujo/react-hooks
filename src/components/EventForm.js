d3b142f407755a4f1269542d71dfd6b2ca734f39
Ryusei Kakujo <ryuseikakujo@gmail.com>
2020-07-30 18:14:31 +0900

    combine reducer

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -62,7 +62,7 @@ const EventForm = () => {
         <button
           className="btn btn-danger"
           onClick={deleteAllEvents}
-          disabled={state.length === 0}
+          disabled={state.events.length === 0}
         >
           Delete all event
         </button>
