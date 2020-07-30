c1dd415246708aaf775ffd2117945caee51db516
Ryusei Kakujo <ryuseikakujo@gmail.com>
2020-07-30 17:03:11 +0900

    Employ useContext

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,19 @@ import reducer from "../reducers";
 
 import EventForm from "./EventForm";
 import Events from "./Events";
+import AppContext from "../contexts/AppContext";
 
+console.log({ AppContext });
 const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-    </div>
+    <AppContext.Provider value={"Hello"}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 };
 
