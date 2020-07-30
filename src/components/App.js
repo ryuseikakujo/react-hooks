01b562531c75937b4e5e3ec84f430bc3b30fc7d8
Ryusei Kakujo <ryuseikakujo@gmail.com>
2020-07-30 11:06:51 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import reducer from "../reducers";
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addEvent = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "CREATE_EVENT",
+      title,
+      body,
+    });
+    setTitle("");
+    setBody("");
+  };
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">Title</label>
-          <input className="form-control" id="formEventTitle" />
+          <input
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="formEventBody">Body</label>
-          <textarea className="form-control" id="formEventBody" />
+          <textarea
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
         </div>
-        <button className="btn btn-primary">Delete event</button>
+        <button className="btn btn-primary" onClick={addEvent}>
+          Create event
+        </button>
         <button className="btn btn-danger">Delete all event</button>
       </form>
 
